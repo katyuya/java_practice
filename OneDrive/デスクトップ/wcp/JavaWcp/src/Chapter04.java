b0a853073c18e7095db03807ad6f9c0b3bc8284b
katyuya <93420930+katyuya@users.noreply.github.com>
2022-06-21 00:13:33 +0900

    [add]java勉強の追加

---
+++ OneDrive/デスクトップ/wcp/JavaWcp/src/Chapter04.java
@@ -0,0 +1,11 @@
+
+public class Chapter04 {
+	public static void main(String[] args) {
+		final int number = 6;
+		String name = "下原賢也";
+		System.out.println(name);
+		name = "自由";
+		System.out.println(name);
+		System.out.println(number);
+	}
+}
