c6c1bfd4f90b75a58e1488f915da5447cc997a3b
katyuya <93420930+katyuya@users.noreply.github.com>
2022-07-23 01:11:42 +0900

    [add]progateでのJava勉強の追加

--- OneDrive/デスクトップ/wcp/JavaWcp/src/progate03.java
+++ OneDrive/デスクトップ/wcp/JavaWcp/src/progate03.java
@@ -1,4 +1,14 @@
 
 public class progate03 {
+	  public static void main(String[] args) {
+	    // printDataメソッドを呼び出してください
+	    printData();
+	    
+	  }
+	  public static void printData(){
+	  // printDataメソッドを定義してください
+	  System.out.println("私の名前はKate Jonesです");
+	  }
+	}
 
 }
